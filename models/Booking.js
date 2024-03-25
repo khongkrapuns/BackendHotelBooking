b9057b961e6e7c94df06ac206c3b25fd70c095c9
khongkrapuns <khongkrapuns@gmail.com>
2024-03-25 23:24:58 +0700

    addroomtype

--- models/Booking.js
+++ models/Booking.js
@@ -9,6 +9,10 @@ const BookingSchema = new mongoose.Schema({
         type: Date,
         required: [true, 'Please add a booking end date']
     },
+    roomType: {
+        type: string,
+        required: [true, 'Please select room type']
+    },
     user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
